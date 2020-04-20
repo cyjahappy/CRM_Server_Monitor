8d0fb68be98165e84a8b61eee075b64fd300936f
CHEN YUANJUN <821659763@qq.com>
2020-04-20 11:15:24 +0800

    加入了定时清理PingResults数据库的功能

--- myapp/clean_database.py
+++ myapp/clean_database.py
@@ -1,6 +1,6 @@
 # 使用datetime模块中的datetime类
 from datetime import datetime, timedelta
-from .models import ServerInfo
+from .models import ServerInfo, PingResults
 
 # 自动清理数据库中今日日期-2天的那天当天的所有数据
 number_of_days = 2
@@ -21,12 +21,17 @@ def clean_database():
     date_to_clean_day = (date_to_clean.strftime('%d'))
 
     # 获取当天数据的QuerySet
-    data = ServerInfo.objects.filter(date__year=date_to_clean_year,
-                                     date__month=date_to_clean_month,
-                                     date__day=date_to_clean_day)
+    ServerInfoData = ServerInfo.objects.filter(date__year=date_to_clean_year,
+                                               date__month=date_to_clean_month,
+                                               date__day=date_to_clean_day)
+    PingResultsData = PingResults.objects.filter(date__year=date_to_clean_year,
+                                                 date__month=date_to_clean_month,
+                                                 date__day=date_to_clean_day)
 
     # 如果QuerySet不为空, 则删除QuerySet对应的数据
-    if data.exists():
-        data.delete()
+    if ServerInfoData.exists():
+        ServerInfoData.delete()
+    if PingResultsData.exists():
+        PingResultsData.delete()
 
     return
