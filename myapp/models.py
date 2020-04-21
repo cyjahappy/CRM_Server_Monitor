08dddb11987ba42129d26e1ef29572fcd86f78e1
CHEN YUANJUN <821659763@qq.com>
2020-04-21 14:38:24 +0800

    添加了带宽的阈值

--- myapp/models.py
+++ myapp/models.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 
+# 这个模型暂时没用
 class ServerInfoThreshold(models.Model):
     id = models.AutoField(primary_key=True)
     server_name = models.CharField(default="", max_length=20)
@@ -12,8 +13,17 @@ class ServerInfoThreshold(models.Model):
     disk_threshold = models.FloatField(default=99.0)
 
 
+# 存储系统各项阈值的数据
+class FullServerInfoThreshold(models.Model):
+    id = models.AutoField(primary_key=True)
+    server_name = models.CharField(default="", max_length=20)
+    cpu_threshold = models.FloatField(default=99.0)
+    memory_threshold = models.FloatField(default=99.0)
+    disk_threshold = models.FloatField(default=99.0)
+    bandwidth_threshold = models.FloatField(default=99.0)
 
 
+# 存储系统各项指标的数据
 class ServerInfo(models.Model):
     date = models.DateTimeField(primary_key=True, auto_now=True)
     cpu = models.FloatField(null=True)
@@ -24,11 +34,13 @@ class ServerInfo(models.Model):
     network_sent = models.FloatField(null=True)
 
 
+# 存储需要Ping的列表
 class PingList(models.Model):
     server_ip = models.GenericIPAddressField(primary_key=True)
     server_name = models.CharField(null=True, max_length=20)
 
 
+# 存储Ping的结果
 class PingResults(models.Model):
     id = models.AutoField(primary_key=True)
     server_ip = models.ForeignKey(PingList, on_delete=models.CASCADE)
--- myapp/server_info_threshold.py
+++ myapp/server_info_threshold.py
@@ -1,20 +1,24 @@
-from .models import ServerInfoThreshold
+from .models import FullServerInfoThreshold
 
 # 从数据库中获取CPU使用率阈值
-cpu_threshold = ServerInfoThreshold.objects.get(id=1).cpu_threshold
+cpu_threshold = FullServerInfoThreshold.objects.get(id=1).cpu_threshold
 
 # 从数据库中获取内存使用率阈值
-memory_threshold = ServerInfoThreshold.objects.get(id=1).memory_threshold
+memory_threshold = FullServerInfoThreshold.objects.get(id=1).memory_threshold
 
 # 从数据库中获取磁盘使用率阈值
-disk_threshold = ServerInfoThreshold.objects.get(id=1).disk_threshold
+disk_threshold = FullServerInfoThreshold.objects.get(id=1).disk_threshold
+
+# 从数据库中获取带宽的阈值
+bandwidth_threshold = FullServerInfoThreshold.objects.get(id=1).bandwidth_threshold
 
 
 def get_server_info_threshold(info=None):
     server_info_threshold = {
         'cpu_threshold': float(cpu_threshold),
         'memory_threshold': float(memory_threshold),
-        'disk_threshold': float(disk_threshold)
+        'disk_threshold': float(disk_threshold),
+        'bandwidth_threshold': float(bandwidth_threshold)
     }
 
     if info:
@@ -25,15 +29,19 @@ def get_server_info_threshold(info=None):
 
 # 更新数据库中各项服务器报警阈值
 def set_cpu_threshold(threshold):
-    ServerInfoThreshold.objects.filter(id=1).update(cpu_threshold=threshold)
+    FullServerInfoThreshold.objects.filter(id=1).update(cpu_threshold=threshold)
 
 
 def set_memory_threshold(threshold):
-    ServerInfoThreshold.objects.filter(id=1).update(memory_threshold=threshold)
+    FullServerInfoThreshold.objects.filter(id=1).update(memory_threshold=threshold)
 
 
 def set_disk_threshold(threshold):
-    ServerInfoThreshold.objects.filter(id=1).update(disk_threshold=threshold)
+    FullServerInfoThreshold.objects.filter(id=1).update(disk_threshold=threshold)
+
+
+def set_bandwidth_threshold(threshold):
+    FullServerInfoThreshold.objects.filter(id=1).update(bandwidth_threshold=threshold)
 
 
 # 每次更新数据库中的数据后会调用该函数来用数据库中的值来刷新全局变量中的数值
@@ -41,6 +49,8 @@ def refresh_data():
     global cpu_threshold
     global memory_threshold
     global disk_threshold
-    cpu_threshold = ServerInfoThreshold.objects.get(id=1).cpu_threshold
-    memory_threshold = ServerInfoThreshold.objects.get(id=1).memory_threshold
-    disk_threshold = ServerInfoThreshold.objects.get(id=1).disk_threshold
+    global bandwidth_threshold
+    cpu_threshold = FullServerInfoThreshold.objects.get(id=1).cpu_threshold
+    memory_threshold = FullServerInfoThreshold.objects.get(id=1).memory_threshold
+    disk_threshold = FullServerInfoThreshold.objects.get(id=1).disk_threshold
+    bandwidth_threshold = FullServerInfoThreshold.objects.get(id=1).bandwidth_threshold
